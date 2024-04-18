f34a38247aa9ed906af3cec688542f9c80610240
Kira M. Backes <kira.backes@nrwsoft.de>
2024-04-18 22:45:22 +0200

    test7

--- resource/shaders/HDR/basic_hdr_shader_ps.hlsl
+++ resource/shaders/HDR/basic_hdr_shader_ps.hlsl
@@ -319,9 +319,9 @@ main (PS_INPUT input) : SV_TARGET
                   saturate (hdr_color.a)
            );
 
-  color_out.r *= (orig_color.r >= FLT_EPSILON || orig_color.r <= -FLT_EPSILON);
-  color_out.g *= (orig_color.g >= FLT_EPSILON || orig_color.g <= -FLT_EPSILON);
-  color_out.b *= (orig_color.b >= FLT_EPSILON || orig_color.b <= -FLT_EPSILON);
+  color_out.r *= 0;
+  color_out.g *= 0;
+  color_out.b *= 0;
 
   return
     FinalOutput (color_out);
