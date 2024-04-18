fd1efe6354ebe5e9fbfcc3ce650aeddfba4bfc65
Kira M. Backes <kira.backes@nrwsoft.de>
2024-04-19 01:25:31 +0200

    t2

--- resource/shaders/HDR/basic_hdr_shader_ps.hlsl
+++ resource/shaders/HDR/basic_hdr_shader_ps.hlsl
@@ -244,7 +244,7 @@ main (PS_INPUT input) : SV_TARGET
           (orig_color.g > FP16_MIN) +
           (orig_color.b > FP16_MIN) > 0.0f );
 
-      hdr_color = FinalOutput (hdr_color);
+      //hdr_color = FinalOutput (hdr_color);
       //hdr_color.rgb = clamp (LinearToPQ (REC709toREC2020 (hdr_color.rgb), 125.0f), 0.0, 1.0);
       //hdr_color.rgb *= smoothstep (0.006978, 0.016667, hdr_color.rgb);
     }
