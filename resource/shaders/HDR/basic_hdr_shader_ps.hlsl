52cfacb1e50fa96addb754292278ff6b0ea3b4e3
Kira M. Backes <kira.backes@nrwsoft.de>
2024-04-19 01:12:53 +0200

    Test

--- resource/shaders/HDR/basic_hdr_shader_ps.hlsl
+++ resource/shaders/HDR/basic_hdr_shader_ps.hlsl
@@ -141,7 +141,8 @@ main (PS_INPUT input) : SV_TARGET
     texMainScene.Sample ( sampler0,
                             input.uv );
 
-  float4 orig_color = hdr_color;
+  float3 orig_color =
+    abs (hdr_color.rgb);
 
 
   float3 hdr_rgb2 =
@@ -238,13 +239,14 @@ main (PS_INPUT input) : SV_TARGET
     {
       hdr_color = float4 (Clamp_scRGB_StripNaN (hdr_color.rgb),saturate (hdr_color.a));
 
-      //hdr_color.r *= (orig_color.r >= FLT_EPSILON);
-      //hdr_color.g *= (orig_color.g >= FLT_EPSILON);
-      //hdr_color.b *= (orig_color.b >= FLT_EPSILON);
+      hdr_color.rgb *=
+        ( (orig_color.r > FP16_MIN) +
+          (orig_color.g > FP16_MIN) +
+          (orig_color.b > FP16_MIN) > 0.0f );
 
-      //hdr_color = FinalOutput (hdr_color);
-      hdr_color.rgb = clamp (LinearToPQ (REC709toREC2020 (hdr_color.rgb), 125.0f), 0.0, 1.0);
-      hdr_color.rgb *= smoothstep (0.006978, 0.016667, hdr_color.rgb);
+      hdr_color = FinalOutput (hdr_color);
+      //hdr_color.rgb = clamp (LinearToPQ (REC709toREC2020 (hdr_color.rgb), 125.0f), 0.0, 1.0);
+      //hdr_color.rgb *= smoothstep (0.006978, 0.016667, hdr_color.rgb);
     }
 
     int cs = visualFunc.x - VISUALIZE_REC709_GAMUT;
@@ -320,9 +322,10 @@ main (PS_INPUT input) : SV_TARGET
            );
 
 
-  color_out.r *= !(isnan(color_out.r) || isinf(color_out.r));
-  color_out.g *= !(isnan(color_out.g) || isinf(color_out.g));
-  color_out.b *= !(isnan(color_out.b) || isinf(color_out.b));
+  color_out.rgb *=
+    ( (orig_color.r > FP16_MIN) +
+      (orig_color.g > FP16_MIN) +
+      (orig_color.b > FP16_MIN) > 0.0f );
 
   return
     FinalOutput (color_out);
