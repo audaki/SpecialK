adc07702f41bcce3886693a1a0a7b2e0781dadbc
Kira M. Backes <kira.backes@nrwsoft.de>
2024-04-19 01:50:28 +0200

    multiply with true

--- resource/shaders/HDR/basic_hdr_shader_ps.hlsl
+++ resource/shaders/HDR/basic_hdr_shader_ps.hlsl
@@ -239,10 +239,7 @@ main (PS_INPUT input) : SV_TARGET
     {
       //hdr_color = float4 (Clamp_scRGB_StripNaN (hdr_color.rgb),saturate (hdr_color.a));
 
-      //hdr_color.rgb *=
-      //  ( (orig_color.r > FP16_MIN) +
-      //    (orig_color.g > FP16_MIN) +
-      //    (orig_color.b > FP16_MIN) > 0.0f );
+      hdr_color.rgb *= ( 1.0f > 0.0f );
 
       //hdr_color = FinalOutput (hdr_color);
       //hdr_color.rgb = clamp (LinearToPQ (REC709toREC2020 (hdr_color.rgb), 125.0f), 0.0, 1.0);
